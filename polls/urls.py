2c0b7eb057ba2f542bce4daa4cd706d20162b48f
Mostafa Hisham <mostafa@Mostafas-MacBook-Pro.local>
2020-08-21 19:15:12 +0200

    Added charts

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('resultsdata/<str:obj>/', views.resultsData, name="resultsdata"),
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
